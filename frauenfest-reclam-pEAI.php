02392c4db48e0c979290178e3f41453463639d3c
Rodrigo Roman <romangarciarod@outlook.com>
2018-03-12 18:05:29 +0000

    Create frauenfest-reclam-pEAI.php

---
+++ frauenfest-reclam-pEAI.php
@@ -0,0 +1,15 @@
+<?php $v = "Verwandter";
+$e = "Euripides";
+$t = "Das Frauenfest";
+$a = "Aristophanes";
+$tr = "Niklas Holzberg";
+$ed = "Philipp Reclam jun. Stuttgart";
+$eds = "Reclam";
+$f = "2011";
+
+$r = "<p>$a<br>$t<hr>$eds</p><!-- Salto de página -->No copiada<!-- Es lo mismo que la portada --><!-- Salto de página --><h2>$a</h2><h1>$t</h1>
+<h3>Traducción [al alemán del griego clásico imagino] y comentarios por $tr</h3>
+<div class="pieDePagina">$ed</ed><!-- Salto hasta la página 42, que leido al revés son la horas que tiene un día. -->
+<blockquote>
+VERWANDTER 
+  >>Die Erde meiner Väter ist nicht unbekannt, 's [este apóstrofe no se de donde viene o que significa en alemán] ist Sparta, und meine Vater ist Tyndareos.<<"; // Por hacer, mejorar el formato. Los espartanos son unos putos asesinos de gente con problemas hereditarios. Hay un tipo que escribe para la universidad de Standford que ha escrito un libro sobre la alzada y caida de la Grecia Clásica que no tiene ni puta idea de Aristófanes, Eurípedes o cualquiera que merezca la pena leer. Es un gilipollas. Opinión personal. En este foro abierto a cualquiera con dos dedos de frente. Si quieres aprender sobre la Grecia clásica, te fías de los originales no putas estadisticas hechas por vete tu a saber quién. El dominical del FINANCIAL TIMES es la mayor puta mierda que he comprado nunca después de un 8 de Marzo. Steve JOBS era un refugiado sirio en CUPERTINO. Aquí donde vivo, lo hay que valen mil veces más. PARA COPIAR ESTILOS, los hay que son muy tontos." ME IMPORTA TRES POLLAS EN VINAGRE muchas cosas. Estoy hasta la polla de los ignorantes JAMES HOLDEN TAMBIÉN. IDIOTS ARE GANANDO. Congratulations Alchimia FOLLOWERS.
